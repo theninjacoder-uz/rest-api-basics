0484aa2dd7a6014638963ce970b028aa643c2b48
User <bekhzod.asadullayev@gmail.com>
2022-09-30 20:32:37 +0500

    configuring datasource for integration testing

--- src/main/java/com/epam/esm/configuration/AppConfiguration.java
+++ src/main/java/com/epam/esm/configuration/AppConfiguration.java
@@ -40,4 +40,5 @@ public class AppConfiguration {
         return new JdbcTemplate(dataSource);
     }
 
+
 }
--- src/test/java/com/epam/esm/configuration/TestConfiguration.java
+++ src/test/java/com/epam/esm/configuration/TestConfiguration.java
@@ -1,2 +1,39 @@
-package com.epam.esm.configuration;public class TestConfiguration {
+package com.epam.esm.configuration;
+
+import com.epam.esm.repository.giftCertificate.GiftCertificateRepoImpl;
+import com.epam.esm.repository.tag.TagRepoImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testDb;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("ninja");
+        dataSource.setPassword("ninja");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public TagRepoImpl tagRepo(JdbcTemplate jdbcTemplate){
+        return new TagRepoImpl(jdbcTemplate);
+    }
+
+    @Bean
+    public GiftCertificateRepoImpl giftCertificateRepo(JdbcTemplate jdbcTemplate){
+        return new GiftCertificateRepoImpl(jdbcTemplate);
+    }
 }
